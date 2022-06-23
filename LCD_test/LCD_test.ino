cba35dd4468ae092051bc4ff96eb5759e2164b56
deepak404found <deepakyadu404@gmail.com>
2022-06-23 18:00:54 +0530

    added BMP & heart rate sensor with motors & lCD

--- LCD_test/LCD_test.ino
+++ LCD_test/LCD_test.ino
@@ -1,18 +1,32 @@
+// Author : Deepak Yadu
+// Circuit desginer: Aman Singh
+
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_BMP085.h>
+#define seaLevelPressure_hPa 1013.25
+
+//BMP sensor
+Adafruit_BMP085 bmp;
+
+//Motor connections
+#define enA 9
+#define in1 6
 
 // Set the LCD address to 0x3F for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 #define REPORTING_PERIOD_MS     1000
 
-float BPM, SpO2;
+int BPM, SpO2;
+float TMP;
 
 PulseOximeter pox;
 uint32_t tsLastReport = 0;
 
 void setup() {
+//  oximeter steup
   Serial.begin(9600);
   pinMode(16, OUTPUT);
   delay(100);
@@ -28,15 +42,39 @@ void setup() {
     Serial.println("SUCCESS");
     lcd.println("SUCCESS");
   }
+
+// BMP pinModes
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
+  }
+
+//  motor pinModes
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
 }
 void loop() {
-  lcd.println("hello");
-  delay(1000);
+
+  //  control motor
+  int potValue = analogRead(A0); // Read potentiometer value
+  int pwmOutput = map(potValue, 0, 1023, 0 , 255); // Map the potentiometer value from 0 to 255
+  analogWrite(enA, pwmOutput); // Send PWM signal to L298N Enable pin
+  Serial.print("pwmOutput: ");
+  Serial.print(pwmOutput);
+  Serial.println("potValue: ");
+  Serial.print(potValue);
+  digitalWrite(in1, HIGH);
+  delay(20);
+
+  //  oximeter and BMP sensor with LCD
   pox.update();
   if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
 
     BPM = pox.getHeartRate();
     SpO2 = pox.getSpO2();
+    TMP = (bmp.readTemperature()*1.8)+32;
 
     Serial.print("BPM: ");
     Serial.println(BPM);
@@ -44,15 +82,24 @@ void loop() {
     Serial.print("SpO2: ");
     Serial.print(SpO2);
     Serial.println("%");
-
+    
+    Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+    
       //lcd print
       lcd.clear();
       lcd.setCursor(0,0);
       lcd.print("BPM: ");
       lcd.print(BPM);
+
+      lcd.setCursor(9,0);
+      lcd.print("TEMP:*F");
+      lcd.setCursor(9,1);
+      lcd.print(TMP);
       
       lcd.setCursor(0,1);
-      lcd.print("SpO2: ");
+      lcd.print("O2: ");
       lcd.print(SpO2);
       lcd.print("%");
 
